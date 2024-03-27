0b12a7763b274fed0aa845bf70acf5b20da8aa37
DariaLuca <andreeadarialuca@gmail.com>
2024-03-27 19:30:22 +0200

    Switch promises to async/ await

--- script.js
+++ script.js
@@ -32,25 +32,30 @@ const categoryCssClasses = {
  */
 document.addEventListener("DOMContentLoaded", async () => {
     const listContainer = document.getElementById(`products-container`);
-    fetch("https://fakestoreapi.com/products")
-            .then((res) =>res.json())
-            .then((products) => {
-                for( const product of products) {
-                    const productContainer = document.createElement(`article`);
-                    productContainer.classList.add(
-                        `col-2-md`,
-                        `m-2`,
-                        `card`,
-                        `cursor-pointer`
-                        );
-                        productContainer.innerHTML = `<img src="${product.image}" alt="Product Image">
-                        <div class="card-content">
-                            <div class="title">${product.title}</div>
-                            <div class="price">$${product.price}</div>
-                            <span class="badge rounded-pill ${categoryCssClasses[product.category]}">${product.category}</span>
-                            <div class="description">${product.description}</div>
-                        </div>`;
-                    listContainer.appendChild(productContainer);
-                }
-            });
-});
+    const products = await getProducts();
+
+     for( const product of products) {
+        const productContainer = document.createElement(`article`);
+         productContainer.classList.add(
+             `col-2-md`,
+             `m-2`,
+             `card`,
+             `cursor-pointer`
+        );
+        productContainer.innerHTML = `<img src="${product.image}" alt="Product Image">
+         <div class="card-content">
+        <div class="title">${product.title}</div>
+        <div class="price">$${product.price}</div>
+        <span class="badge rounded-pill ${categoryCssClasses[product.category]}">${product.category}</span>
+         <div class="description">${product.description}</div>
+         </div>`;
+        listContainer.appendChild(productContainer);
+    }
+    });
+
+
+async function getProducts() {
+    const response = await fetch("https://fakestoreapi.com/products");
+    const products = await response.json();
+    return products;
+};
