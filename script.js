5394fdac9b05fe4a04bce7f3d1d05f2f784bc1da
AlexBlejan <alexandrublejan@yahoo.com>
2024-03-27 19:30:11 +0200

    Swtch promises to async await

--- script.js
+++ script.js
@@ -32,20 +32,14 @@ const categoryCssClasses = {
  */
 document.addEventListener("DOMContentLoaded", async () => {
 	const listContainer = document.getElementById(`products-container`);
-	fetch("https://fakestoreapi.com/products")
-		.then((res) => res.json())
-		.then((products) => {
-			for (const product of products) {
-				const productContainer = document.createElement(`article`);
-				productContainer.classList.add(
-					`col-2-md`,
-					`m-2`,
-					`card`,
-					`cursor-pointer`
-				);
-				productContainer.innerHTML = `<img src="${
-					product.image
-				}" alt="Product Image">
+	const products = await getProducts();
+
+	for (const product of products) {
+		const productContainer = document.createElement(`article`);
+		productContainer.classList.add(`col-2-md`, `m-2`, `card`, `cursor-pointer`);
+		productContainer.innerHTML = `<img src="${
+			product.image
+		}" alt="Product Image">
                 <div class="card-content">
                     <div class="title">${product.title}</div>
                     <div class="price">$${product.price}</div>
@@ -54,7 +48,12 @@ document.addEventListener("DOMContentLoaded", async () => {
 										}">${product.category}</span>
                     <div class="description">${product.description}</div>
                 </div>`;
-				listContainer.appendChild(productContainer);
-			}
-		});
+		listContainer.appendChild(productContainer);
+	}
 });
+
+async function getProducts() {
+	const response = await fetch("https://fakestoreapi.com/products");
+	const products = await response.json();
+	return products;
+}
